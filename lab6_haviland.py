e757619eb15d5a0ad1e0cdf0a6453a6d81a37dbf
haviland7 <shaviland@ufl.edu>
2023-10-24 18:04:45 -0700

    Update lab6_haviland.py

--- lab6_haviland.py
+++ lab6_haviland.py
@@ -1,4 +1,4 @@
-def encoder(message):
+def encode(message):
     result = ''
     for digit in message:
         new_digit = str((int(digit) + 3) % 10)
@@ -17,9 +17,9 @@ def main():
         if choice == "1":
             value = input("Please enter your password to encode: ")
             print("Your password has been encoded and stored!")
-            encoded = encoder(value)
+            encoded = encode(value)
         elif choice == "2":
-            print(f"The encoded password is {encoder(value)} and the original password is {decoder(encoded)}")
+            print(f"The encoded password is {encode(value)} and the original password is {decode(encoded)}")
         elif choice == "3":
             break
         else:

2b6a86201c395f8a0ec20fbef3f99fcc120212a5
haviland7 <shaviland@ufl.edu>
2023-10-24 17:45:13 -0700

    Create lab6_haviland.py

---
+++ lab6_haviland.py
@@ -0,0 +1,28 @@
+def encoder(message):
+    result = ''
+    for digit in message:
+        new_digit = str((int(digit) + 3) % 10)
+        result += new_digit
+    return result
+
+
+def main():
+    while True:
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        choice = input("Please enter an option: ")
+        if choice == "1":
+            value = input("Please enter your password to encode: ")
+            print("Encoded password is", encoder(value))
+            encoded = encoder(value)
+        elif choice == "2":
+            print(f"The encoded password is {encoder(value)} and the original password is {decoder(encoded)}")
+        elif choice == "3":
+            break
+        else:
+            print("Invalid choice")
+        print()
+
+
+main()
